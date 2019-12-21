34e253ec8665da4487bcac47bf0840a2a486c386
mazid <mazidmailbox@gmail.com>
2019-12-21 19:16:18 +0600

    API added for Category

---
+++ src/main/java/com/example/personaldiary/category/CategoryResource.java
@@ -0,0 +1,72 @@
+package com.example.personaldiary.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CategoryResource {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/categories")
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/category/{id}")
+    public Category getCategoryById(@PathVariable long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+
+        if (!category.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found (id-" + id + ")");
+        }
+
+        return category.get();
+    }
+
+    @DeleteMapping("/category/{id}")
+    public void deleteCategoryById(@PathVariable long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+
+        if (!category.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found (id-" + id + ")");
+        }
+
+        categoryRepository.delete(category.get());
+    }
+
+    @PostMapping("/category")
+    public ResponseEntity<Object> createCategory(@RequestBody Category category) {
+        Category savedCategory = categoryRepository.save(category);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedCategory.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/category/{id}")
+    public ResponseEntity<Object> updateCategory(@RequestBody Category category, @PathVariable long id) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        if (!categoryOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        category.setId(id);
+        categoryRepository.save(category);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into category values (10001, 'Home');
+insert into category values (10002, 'Work');
+insert into category values (10003, 'Budget');
+insert into category values (10004, 'Other');
