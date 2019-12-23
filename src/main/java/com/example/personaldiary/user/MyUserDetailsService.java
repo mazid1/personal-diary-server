34d6c7d0cae10b36712fd99136401630137f6464
mazid <mazidmailbox@gmail.com>
2019-12-23 12:10:05 +0600

    removed unused code

--- src/main/java/com/example/personaldiary/user/MyUserDetailsService.java
+++ src/main/java/com/example/personaldiary/user/MyUserDetailsService.java
@@ -1,15 +1,12 @@
 package com.example.personaldiary.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
